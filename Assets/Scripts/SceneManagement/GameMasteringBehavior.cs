d1731993bcc3dc8178529fd1b6ad1e85060a6c21
mizu9972 <oisiimizu9972@icloud.com>
2025-07-26 19:14:11 +0900

    シーンステート管理システム作成

--- Assets/Scripts/SceneManagement/GameMasteringBehavior.cs
+++ Assets/Scripts/SceneManagement/GameMasteringBehavior.cs
@@ -26,6 +26,6 @@ public class GameMasteringBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 }
---
+++ Assets/Scripts/Title/TitleObjectBehavior.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+/// <summary>
+/// タイトル画面用処理
+/// </summary>
+public class TitleObjectBehavior : MonoBehaviour
+{
+
+}
