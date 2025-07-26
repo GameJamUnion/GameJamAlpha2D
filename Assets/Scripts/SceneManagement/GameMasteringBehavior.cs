e27b4d579e2c570f7dcf859a0baaed3fa4efa0c5
mizu9972 <oisiimizu9972@icloud.com>
2025-07-26 14:54:12 +0900

    常駐GameMasteringScene作成

---
+++ Assets/Scripts/SceneManagement/GameMasteringBehavior.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMasteringBehavior : MonoBehaviour
+{
+    private const string GameMasteringSceneName = "GameMastering";
+
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    static void main()
+    {
+        SceneManager.LoadSceneAsync(GameMasteringSceneName, LoadSceneMode.Additive);
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
