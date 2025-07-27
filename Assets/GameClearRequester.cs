c6e6ba3df51af762cd34f7db07c29f3a05248973
mizu9972 <oisiimizu9972@icloud.com>
2025-07-27 14:52:40 +0900

    ゲームクリア画面ボタン入力操作設定

--- Assets/GameClearRequester.cs
+++ Assets/GameClearRequester.cs
@@ -25,7 +25,7 @@ public class GameClearRequester : MonoBehaviour
     {
         if (WorkScore != null)
         {
-            //WorkScore
+            WorkScore.RegisterFullScoreEvent(startGameClear);
         }
     }
 
