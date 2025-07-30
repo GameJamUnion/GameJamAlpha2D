2e3470ebde0aba1aab091711175b8dcb3cae3faf
hozumi_ruy <56009375+ryutaro-matsumoto@users.noreply.github.com>
2025-07-30 22:47:50 +0900

    ユニット移動完了のイベント処理追加

--- Assets/Scripts/workShopScripts/UnitBehaviour/UnitWalk.cs
+++ Assets/Scripts/workShopScripts/UnitBehaviour/UnitWalk.cs
@@ -1,7 +1,5 @@
-using JetBrains.Annotations;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
-using static UnityEngine.RuleTile.TilingRuleOutput;
+using UnityEngine.Events;
 
 public class UnitWalk : MonoBehaviour
 {
@@ -11,9 +9,6 @@ public class UnitWalk : MonoBehaviour
 	[SerializeField]
 	private Animator m_animator;
 
-	[SerializeField]
-	private UnitWalkRoute m_route;
-
 	public enum State {
 		Wait,
 		Walking,
@@ -22,38 +17,40 @@ public class UnitWalk : MonoBehaviour
 
 	private struct MoveInfo {
 		public int nextPointIndex;
-		public int prevPointIndex;
-		public float distance;
 	}
 
+	// 移動情報
 	private MoveInfo m_moveInfo;
 
+	// 状態
 	private State m_state = State.Wait;
 
+	// 移動ルート
 	UnitWalkRoute unitWalkRoute = null;
 
+	UnityAction<GoalPointType> m_onExitMoveEvent;
 
+	/// <summary>
+	/// 到着イベント登録
+	/// </summary>
+	/// <param name="exitMoveEvent"></param>
+	private void RegisterExitMoveEvent(UnityAction<GoalPointType> exitMoveEvent) {
+		m_onExitMoveEvent += exitMoveEvent;
+	}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="route"></param>
 	public void StartWalk(UnitWalkRoute route) {
 		if(unitWalkRoute != null || m_state == State.Walking) { return; }
 		unitWalkRoute = route;
 
 		transform.position = unitWalkRoute.pointList[0];
 
-		m_moveInfo.distance = Vector2.Distance(unitWalkRoute.pointList[0], unitWalkRoute.pointList[1]);
 		m_moveInfo.nextPointIndex = 1;
-		m_moveInfo.prevPointIndex = 0;
 
 		m_state = State.Walking;
-	}
-
-	private void Update() {
-		switch(m_state) {
-			case State.Wait:
-				break;
-			case State.Walking:
-				UpdateWalk();
-				break;
-		}
 	}
 
 	private void UpdateWalk() {
@@ -67,19 +64,36 @@ public class UnitWalk : MonoBehaviour
 		if (direction.magnitude < 0.1f) {
 			transform.position = target;
 			m_moveInfo.nextPointIndex++;
-			m_moveInfo.prevPointIndex++;
 
-
+			// 到達
 			if (m_moveInfo.nextPointIndex >= unitWalkRoute.pointList.Count) {
 				unitWalkRoute = null;
+				m_onExitMoveEvent?.Invoke(unitWalkRoute.goalType);
 				m_state = State.Wait;
 			}
 		}
 	}
 
+	private void Update() {
+		switch(m_state) {
+			case State.Wait:
+				break;
+			case State.Walking:
+				UpdateWalk();
+				break;
+		}
+	}
+
+	#region Test Code
+
+	[Header("Test Property")]
+	[SerializeField]
+	private UnitWalkRoute m_route;
+
 	[ContextMenu("TestRoute")]
 	private void TestRoute() {
 		StartWalk(m_route);
 	}
-	
+
+	#endregion
 }
--- Assets/Scripts/workShopScripts/UnitBehaviour/UnitWalkRoute.cs
+++ Assets/Scripts/workShopScripts/UnitBehaviour/UnitWalkRoute.cs
@@ -7,5 +7,17 @@ public class UnitWalkRoute :ScriptableObject {
     [SerializeField]
     private List<Vector3> m_pointList;
 
+    [SerializeField]
+    GoalPointType m_goalType;
     public List<Vector3> pointList => m_pointList;
+
+    public GoalPointType goalType => m_goalType;
+}
+
+public enum GoalPointType {
+    Work_Section1,  // ì‹Æê1
+	Work_Section2,  // ì‹Æê2
+	Work_Section3,  // ì‹Æê‚P
+	Office,         // ƒIƒtƒBƒX
+    BreakRoom       // ‹xŒeŠ
 }
