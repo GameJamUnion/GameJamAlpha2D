6b0f9b57245e496cacf537c791eb9f042f2fd9d9
hozumi_ruy <56009375+ryutaro-matsumoto@users.noreply.github.com>
2025-07-30 21:32:53 +0900

    アビリティの調整

--- Assets/Scripts/Ability/UnitAbilityCanvasUI.cs
+++ Assets/Scripts/Ability/UnitAbilityCanvasUI.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,12 +11,39 @@ public class UnitAbilityCanvasUI : MonoBehaviour
 	[SerializeField]
 	private List<Transform> m_abilityUIPositions;
 
-	private Canvas m_canvas = null;
+	[SerializeField]
+	private float m_playTime = 0.2f;
+
+	private CanvasGroup m_canvas = null;
 
 	private UnitAbilityUI[] m_unitAbilityUIs = new UnitAbilityUI[IntervieweeUnitAbility.AbilityCapacityMax];
 
 	public bool isDisplay { get; private set; } = false;
 
+
+	/// <summary>
+	/// 指定したユニットのアビリティを表示する
+	/// </summary>
+	/// <param name="unitAbility"></param>
+	public void OnDisplay(IntervieweeUnitAbility unitAbility) {
+		for(int i = 0; i < m_unitAbilityUIs.Length; ++i) {
+			m_unitAbilityUIs[i].SetAbility(unitAbility.abilities[i]);
+		}
+		StartCoroutine(Display(m_playTime));
+
+		isDisplay = true;
+	}
+
+
+	/// <summary>
+	/// 表示を隠す
+	/// </summary>
+	public void OnHide() {
+		StartCoroutine(Hide(m_playTime));
+		isDisplay = false;
+	}
+
+	#region Unity Events
 	private void Awake() {
 		TryGetComponent(out m_canvas);
 
@@ -32,31 +60,34 @@ public class UnitAbilityCanvasUI : MonoBehaviour
 
 			m_unitAbilityUIs[i] = Instantiate<UnitAbilityUI>(m_abilityUIOriginal, m_abilityUIPositions[i]);
 		}
-
-		m_canvas.enabled = false;
 	}
+	#endregion
 
-	/// <summary>
-	/// 指定したユニットのアビリティを表示する
-	/// </summary>
-	/// <param name="unitAbility"></param>
-	public void Display(IntervieweeUnitAbility unitAbility) {
-
-		m_canvas.enabled = true;
-		for(int i = 0; i < m_unitAbilityUIs.Length; ++i) {
-			m_unitAbilityUIs[i].SetAbility(unitAbility.abilities[i]);
+	#region Coroutines
+	private IEnumerator Hide(float playTime) {
+		float elapsedTime = 0.0f;
+		m_canvas.alpha = 1.0f;
+		while (elapsedTime < playTime) {
+			elapsedTime += Time.deltaTime;
+			m_canvas.alpha = Mathf.Lerp(1.0f, 0.0f, elapsedTime / playTime);
+			yield return null;
 		}
-
-		isDisplay = true;
-	}
-
-	/// <summary>
-	/// 表示を隠す
-	/// </summary>
-	public void Hide() {
-		m_canvas.enabled = false;
+		m_canvas.alpha = 0.0f;
 		isDisplay = false;
 	}
+
+	private IEnumerator Display(float playTime) {
+		float elapsedTime = 0.0f;
+		m_canvas.alpha = 0.0f;
+		while (elapsedTime < playTime) {
+			elapsedTime += Time.deltaTime;
+			m_canvas.alpha = Mathf.Lerp(0.0f, 1.0f, elapsedTime / playTime);
+			yield return null;
+		}
+		m_canvas.alpha = 1.0f;
+		isDisplay = true;
+	}
+	#endregion
 
 	[ContextMenu("Test Display")]
 	private void TestDisplay(){
@@ -65,11 +96,11 @@ public class UnitAbilityCanvasUI : MonoBehaviour
 		data.abilities[0] = new Ability();
 		data.abilities[1] = new Ability();
 
-		Display(data);
+		OnDisplay(data);
 	}
 
 	[ContextMenu("Test Hide")]
 	private void TestHide() {
-		Hide();
+		OnHide();
 	}
 }
